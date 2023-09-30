07340a0edb7cfd8abc8cb4fd4ab8bd5b75deb724
Anupam Tripathi <anupamtripathi1433@gmail.com>
2023-09-30 15:51:33 +0530

    Adding one more song

--- MingleTunes/SongConnect/views.py
+++ MingleTunes/SongConnect/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 
 from .models import artist, album, song
 
-def artist_list(request):
-    artists = artist.objects.all()
-    return render(request, 'songss/list.html', {'artists': artists})
+# def artist_list(request):
+#     artists = artist.objects.all()
+#     return render(request, 'songss/list.html', {'artists': artists})
 
-def artist_detail(request, artist_id):
-    artist_obj = artist.objects.get(pk=artist_id)
-    return render(request, 'songss/detail.html', {'artist': artist_obj})
+# def artist_detail(request, artist_id):
+#     artist_obj = artist.objects.get(pk=artist_id)
+#     return render(request, 'songss/detail.html', {'artist': artist_obj})
 
 def index(request):
     allSongs = song.objects.all().order_by('-updated_at')
