4ebf2cf654d8b53ef059d8e1aadb1b548c363884
Anupam Tripathi <anupamtripathi1433@gmail.com>
2023-09-30 15:43:59 +0530

    Using bootstrap modified the html files and showing the song, album and artiist on out website

--- MingleTunes/SongConnect/urls.py
+++ MingleTunes/SongConnect/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index, name="index"),
     path('artists/', views.artist_list, name='artist_list'),
     path('artists/<int:artist_id>/', views.artist_detail, name='artist_detail'),
     # Define similar patterns for other models
--- MingleTunes/SongConnect/views.py
+++ MingleTunes/SongConnect/views.py
@@ -4,8 +4,12 @@ from .models import artist, album, song
 
 def artist_list(request):
     artists = artist.objects.all()
-    return render(request, 'songss/artist_list.html', {'artists': artists})
+    return render(request, 'songss/list.html', {'artists': artists})
 
 def artist_detail(request, artist_id):
     artist_obj = artist.objects.get(pk=artist_id)
-    return render(request, 'songss/artist_detail.html', {'artist': artist_obj})
+    return render(request, 'songss/detail.html', {'artist': artist_obj})
+
+def index(request):
+    allSongs = song.objects.all().order_by('-updated_at')
+    return render(request, template_name="try/index.html", context={"allSongs" : allSongs})
