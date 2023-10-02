beb50f91244717ac431db1252d3c6bfe69c50849
Anupam Tripathi <anupamtripathi1433@gmail.com>
2023-10-02 21:52:07 +0530

    I am currently working on this and developing an approach to make the website more interactive while displaying all the data

--- MingleTunes/SongConnect/urls.py
+++ MingleTunes/SongConnect/urls.py
@@ -3,7 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    # path('artists/', views.artist_list, name='artist_list'),
-    # path('artists/<int:artist_id>/', views.artist_detail, name='artist_detail'),
-    # Define similar patterns for other models
 ]
--- MingleTunes/SongConnect/views.py
+++ MingleTunes/SongConnect/views.py
@@ -2,14 +2,6 @@ from django.shortcuts import render
 
 from .models import artist, album, song
 
-# def artist_list(request):
-#     artists = artist.objects.all()
-#     return render(request, 'songss/list.html', {'artists': artists})
-
-# def artist_detail(request, artist_id):
-#     artist_obj = artist.objects.get(pk=artist_id)
-#     return render(request, 'songss/detail.html', {'artist': artist_obj})
-
 def index(request):
     allSongs = song.objects.all().order_by('-updated_at')
-    return render(request, template_name="try/index.html", context={"allSongs" : allSongs})
+    return render(request, template_name="mingletunes/index.html", context={"allSongs" : allSongs})
