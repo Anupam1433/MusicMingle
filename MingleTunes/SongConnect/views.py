56478f7bde134a1ddead32f7f104367c100ce32b
Anupam Tripathi <anupamtripathi1433@gmail.com>
2023-10-05 00:12:00 +0530

    Updated views and urls

--- MingleTunes/SongConnect/views.py
+++ MingleTunes/SongConnect/views.py
@@ -12,18 +12,23 @@ from .models import PlaylistSong, playlist, interaction, song, album, artist
 
 def index(request):
     # Retrieve objects from different models
-    playlist_songs = PlaylistSong.objects.all()
-    playlists = playlist.objects.all().order_by('-created_at')
-    interactions = interaction.objects.all().order_by('-created_at')
-    songs = song.objects.all().order_by('-updated_at')
+    # playlist_songs = PlaylistSong.objects.all()
+    # playlists = playlist.objects.all().order_by('-created_at')
+    # interactions = interaction.objects.all().order_by('-created_at')
+    allSongs = song.objects.all().order_by('-updated_at')
     
 
     # Combine the objects into a single list
-    allSongs = list(playlist_songs) + list(playlists) + list(interactions) + list(songs)
+    # allSongs = list(playlist_songs) + list(playlists) + list(interactions) + list(songs)
 
     # Sort the combined list by a common field like 'created_at' or 'updated_at'
     # all_content.sort(key=lambda x: x.created_at if hasattr(x, 'created_at') else x.updated_at, reverse=True)
 
     return render(request, template_name="mingletunes/index.html", context={"allSongs": allSongs})
+
+def artistDetails(request):
+    artistSong = song.objects.all()
+    return render(request,template_name="mingletunes/details.html", context={"attistSong": artistSong})
+
 
 
